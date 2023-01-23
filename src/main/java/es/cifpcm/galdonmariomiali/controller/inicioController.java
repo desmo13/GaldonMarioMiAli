a5e7d10a14b281b04ba0166145e1d01c09b738c9
Mario galdon <mariodesmo13@gmail.com>
2023-01-23 09:47:06 +0000

    arreglado error de la vista

--- src/main/java/es/cifpcm/galdonmariomiali/controller/inicioController.java
+++ src/main/java/es/cifpcm/galdonmariomiali/controller/inicioController.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Controller
 public class inicioController {
     @Autowired
@@ -31,19 +36,27 @@ public class inicioController {
         return "Producto";
     }
 
-    @RequestMapping("/create")
+    @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("Municipios",municipioOfferRepository.findAll());
         return "create";
     }
 
     @RequestMapping("/save")
-    public String save(@RequestParam String prodName,@RequestParam int Municipio, @RequestParam float prodPrice, @RequestParam Integer prodStock, @RequestParam String prodImage) {
+    public String save(@RequestParam String prodName,@RequestParam int Municipio, @RequestParam float prodPrice, @RequestParam Integer prodStock, @RequestParam("prodImage") MultipartFile prodImage) {
         Productoffer product = new Productoffer();
+        Path rootLocation = Paths.get("src/main/resources/static/Imagenes");
+        try {
+            Files.copy(prodImage.getInputStream(), rootLocation.resolve(prodImage.getOriginalFilename()));
+
+        } catch ( IOException e) {
+            e.printStackTrace();
+        }
+        String nombreProducto =prodImage.getOriginalFilename();
         product.setProductId((int) productOfferRepository.count());
         product.setProductName(prodName);
         product.setProductPrice(prodPrice);
-        product.setProductPicture(prodImage);
+        product.setProductPicture(nombreProducto);
         product.setProductStock(prodStock);
         product.setIdMunicipio(Municipio);
         productOfferRepository.save(product);
@@ -72,12 +85,20 @@ public class inicioController {
     }
 
     @RequestMapping("/update")
-    public String update(@RequestParam Long id, @RequestParam String prodName,@RequestParam int Municipio, @RequestParam float prodPrice, @RequestParam Integer prodStock, @RequestParam("file") MultipartFile prodImage) {
+    public String update(@RequestParam Long id, @RequestParam String prodName,@RequestParam int Municipio, @RequestParam float prodPrice, @RequestParam Integer prodStock, @RequestParam("prodImage") MultipartFile prodImage) throws IOException {
         Productoffer product = productOfferRepository.findById(id).orElse(null);
+        Path rootLocation = Paths.get("src/main/resources/static/Imagenes");
+        try {
+             Files.copy(prodImage.getInputStream(), rootLocation.resolve(prodImage.getOriginalFilename()));
+
+        } catch ( IOException e) {
+            e.printStackTrace();
+        }
+        String nombreProducto =prodImage.getOriginalFilename();
         product.setProductId((int) productOfferRepository.count());
         product.setProductName(prodName);
         product.setProductPrice(prodPrice);
-        product.setProductPicture(prodImage);
+        product.setProductPicture(nombreProducto);
         product.setProductStock(prodStock);
         product.setIdMunicipio(Municipio);
 
--- src/main/java/es/cifpcm/galdonmariomiali/dao/MunicipioOfferRepository.java
+++ src/main/java/es/cifpcm/galdonmariomiali/dao/MunicipioOfferRepository.java
@@ -1,7 +1,8 @@
 package es.cifpcm.galdonmariomiali.dao;
 
+import es.cifpcm.galdonmariomiali.model.Municipio;
 import es.cifpcm.galdonmariomiali.model.Productoffer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MunicipioOfferRepository extends JpaRepository<Productoffer, Long> {
+public interface MunicipioOfferRepository extends JpaRepository<Municipio, Long> {
 }
