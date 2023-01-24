47e2beccc6647a0fb21361b0f826aa44c1f95e83
Mario galdon <mariodesmo13@gmail.com>
2023-01-24 09:44:16 +0000

    funcion de encripta creada

--- src/main/java/es/cifpcm/galdonmariomiali/controller/UsersController.java
+++ src/main/java/es/cifpcm/galdonmariomiali/controller/UsersController.java
@@ -1,11 +1,14 @@
 package es.cifpcm.galdonmariomiali.controller;
 
+import com.google.common.hash.Hashing;
 import es.cifpcm.galdonmariomiali.dao.ProductOfferRepository;
 import es.cifpcm.galdonmariomiali.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.nio.charset.StandardCharsets;
 
 @Controller
 public class UsersController {
@@ -16,5 +19,8 @@ public class UsersController {
     public String getUsers(Model model){
         model.addAttribute("users",userRepository.findAll());
         return"Usuarios/users";
+    }
+    private String getSHA256(String data) {
+        return Hashing.sha256().hashString(data, StandardCharsets.UTF_8).toString();
     }
 }
