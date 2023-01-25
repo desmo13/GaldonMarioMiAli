4c5920d0570ac32f168f433c41c4d8e2ab330b84
Mario galdon <mariodesmo13@gmail.com>
2023-01-25 10:12:28 +0000

    provando cosas en ajax3

--- src/main/resources/static/js/selector.js
+++ src/main/resources/static/js/selector.js
@@ -7,5 +7,7 @@ function mostrarMunicipio(e){
     //document.getElementById("Municipio").querySelectorAll(".p"+e.target.value).forEach(ele=>{
      //   ele.removeAttribute("hidden");
    // });
-    fetch("http://localhost:8080/GaldonMarioMiAli/Producto2",{method:"Post",body: {municipioId:e.target.value}}).then(e=>console.log(e));
+    var formData = new FormData();
+    formData.append("municipioId",e.target.value)
+    fetch("http://localhost:8080/GaldonMarioMiAli/Producto2",{method:"Post",body: formData}).then(e=>console.log(e));
 }
